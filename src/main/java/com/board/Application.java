d5ba317ad75a614872b43ec82c264566dc1bddb1
wasdfg <leejh9711@naver.com>
2024-01-10 23:20:50 +0900

    spring boot 버전 선택 및 내장 was실행 파일 추가

---
+++ src/main/java/com/board/Application.java
@@ -0,0 +1,11 @@
+package com.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){//내장 was실행
+        SpringApplication.run(Application.class,args);
+    }
+}
