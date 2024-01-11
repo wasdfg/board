24dc1b4997b7b34929a4abf9846156977419f744
wasdfg <leejh9711@naver.com>
2024-01-11 16:57:29 +0900

    설명 변겅

--- src/main/java/com/board/Application.java
+++ src/main/java/com/board/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args){//내장 was실행
+    public static void main(String[] args){//내장 WAS 실행
         SpringApplication.run(Application.class,args);
     }
 }
