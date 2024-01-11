40563a9c5e46c5aae37de467d0daca45c02abbe8
wasdfg <leejh9711@naver.com>
2024-01-11 17:56:32 +0900

    url로 출력하는 controller 예제 생성

---
+++ src/main/java/com/board/web/MainController.java
@@ -0,0 +1,15 @@
+package com.board.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    @GetMapping("/main") //getmapping으로 선언한 주소는 ResponseBody으로 println이 아닌 return 값으로 줘야한다
+    @ResponseBody
+    public String index() { //형을 정해진 형식으로 선언해줘야한다 string,int ...
+        System.out.println("index"); //홈페이지에 뜨지 않는다
+        return "welcome to my website";
+    }
+}
