57359c0befc55a4e09e423d4618b87b7886a7de7
wasdfg <leejh9711@naver.com>
2024-01-11 20:16:03 +0900

    Question에 대한 reply 작성을 위한 엔티티 생성

---
+++ src/main/java/com/board/Reply.java
@@ -0,0 +1,30 @@
+package com.board;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Reply {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //id를 순차적으로 증가
+    private Integer id; 
+
+    @Column(columnDefinition = "Text")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne //질문은 하나지만 답변은 여러개가 될수 있기에 N:1관계를 사용
+    private Question question; //Question 엔티티를 참조하기 위해 선언
+
+}
