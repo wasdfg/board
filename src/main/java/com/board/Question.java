440962ba2b8c4e8c77427efefa3555c504647c4d
wasdfg <leejh9711@naver.com>
2024-01-12 21:27:00 +0900

    변수명 이해하기 쉽게 변경

--- src/main/java/com/board/Question.java
+++ src/main/java/com/board/Question.java
@@ -7,12 +7,15 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.CascadeType;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@NoArgsConstructor //기본 생성자 자동추가 public Question{}을 생성해준다
 @Getter
 @Setter
 @Entity //데이터베이스 entity로 사용하기 위해 선언
@@ -20,18 +23,26 @@ import java.util.List;
 public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //generationtype은 값을 자동으로 1씩 증가시켜준다
-    private Integer id; //int와 달리 integer는 null값으로 사용이 가능하여 sql용으로 적절하다. 저장공간도 크다
+    private Integer UploadNumber; //int와 달리 integer는 null값으로 사용이 가능하여 sql용으로 적절하다. 저장공간도 크다
     
-    @Column(length = 200) //길이를 200으로 제한
-    private String subject;
+    @Column(length = 200,nullable = false) //길이를 200으로 제한
+    private String Title;
 
-    @Column(columnDefinition = "TEXT")//해당 컬럼을 TEXT로 정의 하겠다
-    private String content;
+    @Column(columnDefinition = "TEXT",nullable = false)//해당 컬럼을 TEXT로 정의 하겠다
+    private String Content;
 
-    private LocalDateTime now;
+    private LocalDateTime NowTime;
 
     @OneToMany(mappedBy = "question",cascade = CascadeType.REMOVE) //질문이 삭제되면 답변도 같이 삭제되게 cascade를 추가
     private List<Reply> replyList; //reply 객체를 list로 담아둔다.
-                                   //참조하기 위해 question.getReplyList()를 호출한다.
-                                   //mappedBy는 참조하는 엔티티를 선언해준다.
+    //참조하기 위해 question.getReplyList()를 호출한다.
+    //mappedBy는 참조하는 엔티티를 선언해준다.
+
+    @Builder
+    public Question(String Title,String Content,LocalDateTime NowTime) {
+        this.Title = Title;
+        this.Content = Content;
+        this.NowTime = NowTime;
+    }
+
 }
--- src/main/java/com/board/Reply.java
+++ src/main/java/com/board/Reply.java
@@ -7,22 +7,24 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+@NoArgsConstructor
 @Getter
 @Setter
 @Entity
 public class Reply {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //id를 순차적으로 증가
-    private Integer id; 
+    private Integer UploadNumber;
 
     @Column(columnDefinition = "Text")
-    private String content;
+    private String Content;
 
-    private LocalDateTime now;
+    private LocalDateTime NowTime;
 
     @ManyToOne //질문은 하나지만 답변은 여러개가 될수 있기에 N:1관계를 사용
     private Question question; //Question 엔티티를 참조하기 위해 선언
