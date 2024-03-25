eafd96ff2d206479d06d30f2b102988e2404c41f
wasdfg <leejh9711@naver.com>
2024-03-25 15:05:40 +0900

    mustache를 제거하고 thymeleaf를 사용하기 위해 파일 추가,제거 및 전반적인 코드 수정 작업

--- src/main/resources/templates/static/keep.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // 이전에 입력한 내용이 있을 경우, 해당 내용을 textarea에 설정
-    var contentValue = "{{#content}}{{.}}{{/content}}";
-    document.getElementById('content').value = contentValue;
-});
