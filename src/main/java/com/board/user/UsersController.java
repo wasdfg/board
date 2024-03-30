85d1642fba6dd5067699896416df1dc1cf2d8ae5
wasdfg <leejh9711@naver.com>
2024-03-30 19:07:36 +0900

    회원가입 사이트 추가 및 회원가입 검증 기능 추가

---
+++ src/main/java/com/board/user/UsersController.java
@@ -0,0 +1,49 @@
+package com.board.user;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/user")
+public class UsersController {
+    private final UsersService usersService;
+
+    @GetMapping("/signup")
+    public String signup(UsersCreateForm usersCreateForm) {
+        return "signup_form";
+    }
+
+    @PostMapping("/signup")
+    public String signup(@Valid UsersCreateForm usersCreateForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) { 
+            return "signup_form";
+        }
+
+        if (!usersCreateForm.getPassword1().equals(usersCreateForm.getPassword2())) { //비밀번호 검증이 실패했을 경우
+            bindingResult.rejectValue("password2", "passwordInCorrect", "2개의 패스워드가 일치하지 않습니다.");
+            return "signup_form";
+        }
+
+        try {
+        usersService.create(usersCreateForm.getUsername(),usersCreateForm.getPassword1(), usersCreateForm.getEmail());
+        }catch(DataIntegrityViolationException e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+            return "signup_form";
+        }catch(Exception e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", e.getMessage());
+            return "signup_form";
+        }
+
+        return "redirect:/";
+    }
+}
