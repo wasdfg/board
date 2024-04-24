f4bb750fe4d77e2ebe8b44034df5c04f1f90451a
wasdfg <leejh9711@naver.com>
2024-04-24 23:32:20 +0900

    비밀번호를 입력하여 개인정보 변경 페이지로 유도하게 만듦

--- src/main/java/com/board/user/UsersController.java
+++ src/main/java/com/board/user/UsersController.java
@@ -4,17 +4,23 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.security.Principal;
 
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("/user")
 public class UsersController {
     private final UsersService usersService;
+
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping("/signup")
     public String signup(UsersCreateForm usersCreateForm) {
@@ -50,5 +56,26 @@ public class UsersController {
     @GetMapping("/login")
     public String login(){
         return "login_form";
+    }
+
+    @GetMapping("/chginfo")
+    public String changePw(){
+        return "change_pwd";
+    }
+
+    @GetMapping("/chkinfo")
+    public String checkPw(){
+        return "check_pwd";
+    }
+
+    @PostMapping("/chkinfo")
+    public String checkPw(@RequestParam("password") String password,Principal principal){
+        SignUpUser signUpUser = this.usersService.getUser(principal.getName());
+        if(passwordEncoder.matches(password, signUpUser.getPassword())){
+            return changePw();
+        }
+        else {
+            return "check_pwd";
+        }
     }
 }
