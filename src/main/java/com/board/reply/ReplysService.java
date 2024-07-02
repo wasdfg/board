d652c8270ef1ac7fe58216e90330a9f05a3831f4
wasdfg <leejh9711@naver.com>
2024-07-02 21:06:13 +0900

    대댓글에 사용할 부모주소를 확인하는 로직 변경

--- src/main/java/com/board/reply/ReplysService.java
+++ src/main/java/com/board/reply/ReplysService.java
@@ -2,6 +2,7 @@ package com.board.reply;
 
 
 import com.board.question.Questions;
+import com.board.question.QuestionsRepository;
 import com.board.user.SignUpUser;
 import com.board.reply.dto.ReplysBasicDto;
 import lombok.RequiredArgsConstructor;
@@ -21,18 +22,20 @@ import java.util.Optional;
 public class ReplysService {
     private final ReplysRepository replysRepository;
 
+    private final QuestionsRepository questionsRepository;
     public Replys create(Questions questions, String content, SignUpUser author,Replys prereplys){
         Replys replys = new Replys();
         replys.setContent(content);
         replys.setNowtime(LocalDateTime.now());
         replys.setQuestions(questions);
         replys.setAuthor(author);
-        if(prereplys != null && prereplys.getParent_id() != null) {
+        if(prereplys != null) {
             replys.setParent_id(prereplys.getUploadnumber());
         }
         else{
             replys.setParent_id(null);
         }
+        //questions.setReplysListsize(questions.getReplysListsize()+1);
         this.replysRepository.save(replys); //위에 있는 content nowtime questions를 replys에 저장
         return replys;
     }
