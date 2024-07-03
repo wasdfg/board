b994e6226e561b26e2ba2769d75f8189819ce6da
wasdfg <leejh9711@naver.com>
2024-07-03 21:51:00 +0900

    replys의 joincolumn을 추가

--- src/main/java/com/board/reply/Replys.java
+++ src/main/java/com/board/reply/Replys.java
@@ -31,10 +31,11 @@ public class Replys {
     private LocalDateTime modifyDate; //수정된 글의 시간
 
     @ManyToOne(fetch = FetchType.LAZY) //질문은 하나지만 답변은 여러개가 될수 있기에 N:1관계를 사용
-    @JoinColumn(name = "questions_uploadnumber")
+    @JoinColumn(name = "questions_uploadnumber",referencedColumnName = "uploadnumber")
     private Questions questions; //Questions 엔티티를 참조하기 위해 선언
 
     @ManyToOne(fetch = FetchType.LAZY) //글쓴이 1명당 여러 답변을 할 수 있어 다대1로 설정
+    @JoinColumn(name="author_id",referencedColumnName = "id")
     private SignUpUser author;
 
     @ManyToMany(fetch = FetchType.LAZY)
