874ffdb735ac6c74b296c6ecf463d453bf1a0b0d
wasdfg <leejh9711@naver.com>
2024-07-14 22:18:32 +0900

    사용하지 않는 코드 제거

--- src/main/java/com/board/Application.java
+++ src/main/java/com/board/Application.java
@@ -2,7 +2,6 @@ package com.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 //@EnableJpaRepositories
 @SpringBootApplication
