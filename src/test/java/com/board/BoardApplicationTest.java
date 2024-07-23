5a610b4d2d0386b021707f024a45d4f619d05cfd
wasdfg <leejh9711@naver.com>
2024-07-23 16:50:36 +0900

    테스트 코드 변경

--- src/test/java/com/board/BoardApplicationTest.java
+++ src/test/java/com/board/BoardApplicationTest.java
@@ -5,25 +5,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.board.question.Questions;
+import com.board.question.QuestionsForm;
 import com.board.question.QuestionsRepository;
 import com.board.question.QuestionsService;
 import com.board.reply.Replys;
 import com.board.reply.ReplysRepository;
-import com.board.user.SignUpUser;
+import com.board.reply.ReplysService;
 import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-import java.util.Random;
 
 @SpringBootTest
 public class BoardApplicationTest {
@@ -36,6 +33,9 @@ public class BoardApplicationTest {
     @Autowired
     private QuestionsService questionsService;
 
+    @Autowired
+    private ReplysService replysService;
+
     @Test
     void test(){ //테스트 데이터는 h2 인메모리에 저장되지 않는다. test시 test전용 db가 생기기 떄문이다.
         //Questions q1 = new Questions();
@@ -44,7 +44,7 @@ public class BoardApplicationTest {
         //q1.setNowtime(LocalDateTime.now());//질문을 올린 시간
         //this.questionsRepository.save(q1);
 
-        questionsRepository.save(Questions.builder().title("질문 있습니다.").content("이거 뭐 만드는건가요?").nowtime(LocalDateTime.now()).build()); //질문을 builder를 사용하여 저장
+        //questionsRepository.save(Questions.builder().title("질문 있습니다.").content("이거 뭐 만드는건가요?").nowtime(LocalDateTime.now()).build()); //질문을 builder를 사용하여 저장
 
         Questions q2 = new Questions();
         q2.setTitle("질문이요");
@@ -145,14 +145,36 @@ public class BoardApplicationTest {
     }
 
     @Test
-    @Rollback(value = false)
-    @Transactional //테스트 데이터를 인메모리db에 저장되는지 확인
+    //@Rollback(value = false)
+    //@Transactional //테스트 데이터를 인메모리db에 저장되는지 확인
     void testJpa() {
         for (int i = 1; i <= 1000000; i++) {
             String title = String.format("테스트 데이터입니다:[%03d]", i);
             String content = "내용무";
-            String[] arr = {"common","ask"};
+            String[] arr = {"free","ask"};
             this.questionsService.create(title, content,null,arr[(int)((double)arr.length*Math.random())]);
         }
+    }
+
+    @Test
+    void testReplys(){
+        for(int i = 4;i <= 1000004;i++){
+            String content = String.format("양산형 댓글입니다 [%03d]",(int)((double)100*Math.random()));
+            Optional<Questions> questions = this.questionsRepository.findById(i);
+            Questions questions1 = questions.get();
+            this.replysService.create(questions1,content,null,null);
+        }
+    }
+
+    @Test
+    public void testQuestionsForm() {
+        QuestionsForm form = new QuestionsForm();
+        form.setTitle("Test Title");
+        form.setContent("Test Content");
+        form.setCategory("Test Category");
+
+        Assertions.assertNotNull(form.getTitle());
+        Assertions.assertNotNull(form.getContent());
+        Assertions.assertNotNull(form.getCategory());
     }
 }
