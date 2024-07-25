22a4ed7584a0991339b5bfb7df7407dc68ab2df7
wasdfg <leejh9711@naver.com>
2024-07-25 20:54:58 +0900

    외래키의 이름을 직관적으로 수정

--- src/main/java/com/board/question/Questions.java
+++ src/main/java/com/board/question/Questions.java
@@ -34,7 +34,7 @@ public class Questions {
     //mappedBy는 참조하는 엔티티를 선언해준다.
 
     @ManyToOne //글쓴이 1명당 여러 질문을 할 수 있어 다대1로 설정
-    @JoinColumn(name = "username",referencedColumnName = "id")
+    @JoinColumn(name = "user_id",referencedColumnName = "id")
     private Users author;
 
     @ManyToMany
