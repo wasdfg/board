868d9c409609d9b13e13ed04da56925403a91197
wasdfg <leejh9711@naver.com>
2025-04-19 20:48:23 +0900

    dto를 class로 변경한 것에 의한 변수명 재설정

--- src/main/java/com/board/question/dto/QuestionsListDto.java
+++ src/main/java/com/board/question/dto/QuestionsListDto.java
@@ -1,21 +1,35 @@
 package com.board.question.dto;
 
+import lombok.Getter;
+
 import java.time.LocalDateTime;
+import java.util.List;
 
-public interface QuestionsListDto {
-    String getTitle();
+@Getter
+public class QuestionsListDto {
 
-    Integer getUploadnumber();
+    private Integer uploadnumber;
+    private String title;
+    private String content;
+    private LocalDateTime nowtime;
+    private String category;
+    private int view;
+    private int replysSize;
+    private String nickname;
+    private List<String> replyContents;  // 추가된 부분
 
-    LocalDateTime getNowtime();
-
-    Long getUserId();
-
-    String getNickname();
-
-    int getView();
-
-    int getReplysSize();
-
-    String getCategory();
+    public QuestionsListDto(Integer uploadnumber, String title, String content,
+                            LocalDateTime nowtime, String category, int view,
+                            int replysSize, String nickname,
+                            List<String> replyContents) {
+        this.uploadnumber = uploadnumber;
+        this.title = title;
+        this.content = content;
+        this.nowtime = nowtime;
+        this.category = category;
+        this.view = view;
+        this.replysSize = replysSize;
+        this.nickname = nickname;
+        this.replyContents = replyContents;
+    }
 }
