e9ab65028305f885978b877848b007174e3d8734
wasdfg <leejh9711@naver.com>
2025-04-30 23:33:21 +0900

    네이티브쿼리로 작성하기 위해 인터페이스로 변경

--- src/main/java/com/board/question/dto/QuestionsListDto.java
+++ src/main/java/com/board/question/dto/QuestionsListDto.java
@@ -1,35 +1,16 @@
 package com.board.question.dto;
 
-import lombok.Getter;
-
 import java.time.LocalDateTime;
-import java.util.List;
 
-@Getter
-public class QuestionsListDto {
+public interface QuestionsListDto {
 
-    private Integer uploadnumber;
-    private String title;
-    private String content;
-    private LocalDateTime nowtime;
-    private String category;
-    private int view;
-    private int replysSize;
-    private String nickname;
-    private List<String> replyContents;
+    Integer getUploadnumber();
+    String getTitle();
+    String getContent();
+    LocalDateTime getNowtime();
+    String getCategory();
+    int getView();
+    int getReplysCount();
+    String getNickname();
 
-    public QuestionsListDto(Integer uploadnumber, String title, String content,
-                            LocalDateTime nowtime, String category, int view,
-                            int replysSize, String nickname,
-                            List<String> replyContents) {
-        this.uploadnumber = uploadnumber;
-        this.title = title;
-        this.content = content;
-        this.nowtime = nowtime;
-        this.category = category;
-        this.view = view;
-        this.replysSize = replysSize;
-        this.nickname = nickname;
-        this.replyContents = replyContents;
-    }
 }
